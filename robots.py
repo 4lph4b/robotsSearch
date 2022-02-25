525544946acaa403353509e782a37db4cde8379f
4lph4b <50269172+4lph4b@users.noreply.github.com>
2022-02-25 07:43:06 -0800

    Update robots.py

--- robots.py
+++ robots.py
@@ -1,30 +1,59 @@
 #!/usr/bin/python
 
 import requests
-import sys
+import sys, os
+import threading
 
 from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
 
-protos = ['http', 'https']
-ports  = [80,443,8080,8443]
+threadLimiter = threading.BoundedSemaphore(10)
 
-with open('webservers.txt') as f:
+protos = ['http', 'https']
+
+outputDir = "./robots.txt-files"
+inputFile = "./web-domains.txt"
+
+if not os.path.exists(outputDir):
+	os.makedirs(outputDir)
+
+def getRobots(proto, domain):
+	threadLimiter.acquire()
+
+	url = "%s://%s/robots.txt" % (proto, domain)
+
+	try:
+		sys.stdout.write("\033[K%s\r" % url)
+		sys.stdout.flush()
+		r = requests.get(url, verify=False, timeout=1)
+		if r.status_code == 200:
+			print("=== %s ===" % url)
+			f = open("%s/%s.%s.txt" % (outputDir, proto, domain), "w")
+			f.write("###############################\n")
+			f.write("%s\n" % url)
+			f.write("###############################\n")
+			f.write("\n")
+			f.write(r.text)
+			f.close()
+	except requests.exceptions.ConnectionError:
+		pass
+	except requests.exceptions.ReadTimeout:
+		pass
+	except UnicodeEncodeError:
+		pass
+
+	threadLimiter.release()
+
+with open(inputFile) as f:
 	for l in f:
 		line = l.strip()
-
 		for proto in protos:
-			for port in ports:
-				try:
-					url = "%s://%s:%d/robots.txt" % (proto, line, port)
-					sys.stdout.write("\033[K%s\r" % url)
-					sys.stdout.flush()
-					r = requests.get(url, verify=False, timeout=1)
-					if r.status_code == 200:
-						print("=== %s ===" % url)
-						print(r.text)
-						print()
-				except:
-					pass
-
-print("\033[K=== Done! ===")
+			try:
+				threading.Thread(target=getRobots, args=[proto, line]).start()
+			except KeyboardInterrupt:
+				print("\nKeyboard exit")
+				os._exit(1)
+			except Exception as e:
+				print("")
+				print(e)
+				print("")
